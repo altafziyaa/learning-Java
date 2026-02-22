48ab26e4abb11cf77b887f59e38c5b5a256ba0cf
md altaf <altafziyaa91@gmail.com>
2026-02-22 13:19:27 +0530

    Initial Java learning commit

---
+++ dividedBy.java
@@ -0,0 +1,13 @@
+public class dividedBy {
+  public static void main(String[] args) {
+    int num = 22;
+    if (num%5==0) {
+      System.out.println("its divided by 5");
+    } else if (num%11==0) {
+      System.out.println("its divide by 11");
+    }else{
+      System.out.println("not divide by 5 and 11");
+    }
+  }
+
+}
---
+++ divisionBy3.java
@@ -0,0 +1,10 @@
+public class divisionBy3 {
+  public static void main(String[] args) {
+    int num = 27;
+    if (num % 3 == 0) {
+      System.out.println("divided by ");
+    } else {
+      System.out.println(" not divided");
+    }
+  }
+}
