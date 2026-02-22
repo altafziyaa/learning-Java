a98dfa1c6a938a5699a861f8661f875f424ea701
md altaf <altafziyaa91@gmail.com>
2026-02-22 16:47:57 +0530

    new update

---
+++ ifelse/primeNum.java
@@ -0,0 +1,12 @@
+package ifelse;
+
+public class primeNum {
+  public static void main(String[] args) {
+    int num1 = 20;
+    if (num1%2==0) {
+      System.out.println("this is call prime num");
+    } else {
+      System.out.println("this is not prime num");
+    }
+  }
+}
---
+++ switchCase/calculate.java
@@ -0,0 +1,17 @@
+package switchCase;
+
+public class calculate {
+public static void main(String[] args) {
+    int a = 5, b = 2;
+  char op = '+';
+
+  switch(op)
+  {
+    case '+' :
+    System.out.println(a+b);
+    break;
+    case'-':
+    System.out.println(a-b);
+  }
+}
+}
